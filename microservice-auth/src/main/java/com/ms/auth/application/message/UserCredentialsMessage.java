21fb74e8ae3b27c15f3777e9800ec45118551c10
filipemartinsdev <dev.filipemartins@gmail.com>
2026-07-06 10:42:56 -0300

    chore: update docker environment variables

--- microservice-auth/src/main/java/com/ms/auth/application/message/UserCredentialsMessage.java
+++ microservice-auth/src/main/java/com/ms/auth/application/message/UserCredentialsMessage.java
@@ -3,12 +3,11 @@ package com.ms.auth.application.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.UUID;
 
-public record UserCredentialsDTO (
+public record UserCredentialsMessage(
         @NotNull UUID id,
         @NotBlank String username,
         @Email String email
